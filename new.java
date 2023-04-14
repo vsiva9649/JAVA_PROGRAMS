5f010a57a642c67629d29b3d71d68b8444219d19
Sivarama Krishnan V <vsiva9649@gmail.com>
2023-04-14 08:24:36 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,5 +1,5 @@
 public class new {
     public static void main(String[] args) {
-        System.out.println("new Chenges");
+        System.out.println("new Chenges testing priyanga_mam");
     }
 }
