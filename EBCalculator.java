7eddb6dc9aa9fbe51db0253c437d2ff2f5ef3734
vsiva9649 <vsiva9649@gmail.com>
2023-04-14 15:22:19 +0530

    new

--- EBCalculator.java
+++ EBCalculator.java
@@ -3,18 +3,18 @@ class EBCalculator
 	public static void main(String[] args)
 	{
 	EBCalculator reader = new EBCalculator();
-		reader.calculate_units();
-		reader.add();
+	int bill = reader.calculate_units();
+	System.out.println(bill);
+		int amount = 500;
 	}
-	void calculate_units()
+	int calculate_units()
 	{
 		int units = 150;
         int price = 2;
         System.out.println(units*price);
+		return units*price;
+	
+
 	}
-	void add()
-	{
-		System.out.println(10+20);
-		
-	}
+	
 }
